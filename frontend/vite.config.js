b0c3d0b1fdd9c7ddd0a4e43040d3fb71c9af874f
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2025-10-26 00:21:21 +0530

    hardcoded

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:5001/api'),
+  },
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
-    }
+    port: 3000
   }
 })
