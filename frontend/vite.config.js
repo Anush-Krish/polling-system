707eb1c54a00c54ad83364543b3b6841ef481f4a
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2025-10-26 00:25:17 +0530

    hardcoded

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:5001/api'),
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://polling-system-9vqe.onrender.com/api'),
   },
   server: {
     port: 3000
